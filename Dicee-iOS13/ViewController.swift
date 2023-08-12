46ec0e8de1f9c3c144f9c409a384c181ad707d27
Rodrigo S. Gouveia <developer@rodrigo.ooo>
2023-08-12 00:01:50 -0400

    Finished app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+	// Referencing UI Element
+	@IBOutlet weak var diceImageViewLeft: UIImageView!
+	@IBOutlet weak var diceImageViewRight: UIImageView!
+	
+	let diceImages = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+		
+//		Same function, the first line is for Xcode 14
+//		diceImageViewLeft.image = UIImage(named: "DiceSix")
+//		diceImageViewLeft.image = #imageLiteral(resourceName: "DiceSix")
+//		diceImageViewRight.image = UIImage(named: "DiceTwo")
+//		diceImageViewRight.image = #imageLiteral(resourceName: "DiceTwo")
+		
+//		Changing Alpha from image
+		diceImageViewLeft.alpha = 0
+		diceImageViewRight.alpha = 0
     }
 
+	@IBAction func rollButtomPressed(_ sender: UIButton) {
+		print("Buttom got tapped.")
+		
+		diceImageViewLeft.alpha = 1
+		diceImageViewRight.alpha = 1
+		
+		diceImageViewLeft.image = diceImages[Int.random(in: 0...5)]
+		diceImageViewRight.image = diceImages[Int.random(in: 0...5)]
 
+	}
+	
 }
 
